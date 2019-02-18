310901fc73f9cfc584af3b90328d7be590d7270a
mohak1283 <mohak1283@gmail.com>
2019-02-18 11:11:49 +0530

    third

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -11,76 +11,100 @@ class DetailScreen extends StatefulWidget {
 }
 
 class _DetailScreenState extends State<DetailScreen> {
+
+ String updatedUrl = "assets/H.gif";
+ Timer timer;
+
+  @override
+   void initState(){
+    super.initState();
+    
+  }
+
+  String imageUrl;
   @override
   Widget build(BuildContext context) {
- 
-   print("Text Received : ${widget.text}");
+    print("Text Received : ${widget.text}");
 
     return Scaffold(
-      appBar: AppBar(title: Text("Next"),),
-        body: Column(
-          children: <Widget>[
-            Text(widget.text,
-                style: TextStyle(
+      appBar: AppBar(
+        title: Text("Next"),
+      ),
+      body: Column(
+        children: <Widget>[
+          Text(widget.text,
+              style: TextStyle(
                   color: Colors.white,
                   fontSize: 20.0,
-                  fontStyle: FontStyle.italic
-                )),
+                  fontStyle: FontStyle.italic)),
 
-                
+           displayGifImage(widget.text),
 
-              //  displayGifImage(widget.text),
-
-            // FadeInImage(
-            //   image: AssetImage('assets/'),
-            // )    
-          ],
-        ),        );
+          // FadeInImage(
+          //   image: AssetImage('assets/'),
+          // )
+        ],
+      ),
+    );
   }
 
-//   Widget displayGifImage(String data){
-//     print("Text Received in method : ${data}");
-//     if(data.isEmpty){
-//        print("Inside if condition");
-//       return new Padding(
-//         padding: const EdgeInsets.all(8.0),
-//         child: Container(),
-//       );
-//     }else{
-//       print("Inside else condition");
-//     for(int i = 0; i<data.length;i++){
-//      String singleLetter =data[i].toUpperCase();
-//      print("Singleletter : $singleLetter");
-//      String imageUrl = 'assets/'+'${singleLetter}'+'.gif';
-//      print("ImageUrl : $imageUrl");
-    
-//     Timer.periodic(Duration(microseconds: 3000), (Timer t) {
-//    // Runs after every 1000ms
-//       return Padding(
-//         padding: const EdgeInsets.all(8.0),
-//         child: Container(
-//           width: 300.0,
-//           height: 450.0,
-//           decoration: BoxDecoration(
-//               borderRadius: BorderRadius.circular(20.0),
-//               image: DecorationImage(
-//                   image: AssetImage(imageUrl),
-//                   fit: BoxFit.cover)),
-         
-//         ),
-//       );
+  Widget displayGifImage(String data) {
+    print("Text Received in method : ${data}");
+    if (data.isEmpty) {
+      print("Inside if condition");
+      return new Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(),
+      );
+    } else {
+      print("Inside else condition");
+      for (int i = 0; i < data.length; i++) {
+        String singleLetter = data[i].toUpperCase();
+        print("Singleletter : $singleLetter");
+         imageUrl = 'assets/' + '${singleLetter}' + '.gif';
+        print("ImageUrl : $imageUrl");
 
-// });
+         return new Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          width: 300.0,
+          height: 450.0,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20.0),
+            image: DecorationImage(
+              image: AssetImage(updatedUrl) , fit: BoxFit.cover
+            )
+          ),
+        ),
+      );
+       
 
+        new Timer.periodic(Duration(microseconds: 3000), (Timer t) {
 
-//    }
-//     }
+          timer = t;
+          setState(() {
+           updatedUrl =imageUrl; 
+          });
+          
+          // Runs after every 1000ms
+          // return Padding(
+          //   padding: const EdgeInsets.all(8.0),
+          //   child: Container(
+          //     width: 300.0,
+          //     height: 450.0,
+          //     decoration: BoxDecoration(
+          //         borderRadius: BorderRadius.circular(20.0),
+          //         image: DecorationImage(
+          //             image: AssetImage(imageUrl), fit: BoxFit.cover)),
+          //   ),
+          // );
+        });
 
-   
-
-//   }
-
- 
+      }
+      timer.cancel();
+      
+     
+      
+   }
   }
-
-
+}
